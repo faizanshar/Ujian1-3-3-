7caa51b641d403e1a70f5f01390a33b3f6cac8f6
faizanshar <muhammadfaizanshar@gmai.com>
2020-09-26 13:45:19 +0700

    Ujian1-3(3)

---
+++ ujian3(3).js
@@ -0,0 +1,9 @@
+function luas(p,l){
+    total = p * l;
+    return total
+}
+var p = prompt("Masukkan Panjang!")
+var l = prompt("Masukkan Lebar!")
+luas(p,l)
+alert(`Luas Persegi Panjangnya adalah ${total}`)
+document.write(`Panjang : ${p} <br> Lebar : ${l} <br> Luas Persegi Panjang : ${total}`)
